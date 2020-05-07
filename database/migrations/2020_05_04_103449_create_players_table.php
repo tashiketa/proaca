0d9c6c5ab26c010f7169b2056aa9a0102db5c4d1
tashiketa <takeshita@lassic.co.jp>
2020-05-07 05:59:52 +0000

    migration

---
+++ database/migrations/2020_05_04_103449_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('photo');
+            $table->integer('team_id')->unsigned()->index();
+            $table->integer('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
